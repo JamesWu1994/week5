988e5294a2b91d6e4b3beed81f8f3d502e97ea18
James Wu <wujames87@gmail.com>
2021-03-20 15:49:57 -0700

    Create LoggerInterface.java

---
+++ LoggerInterface.java
@@ -0,0 +1,9 @@
+
+public interface LoggerInterface {
+	public interface Logger {
+	    //the interface with both abstract functions.
+	    public void Log(String message);
+	    public void Error(String message);
+	    
+	}
+}
